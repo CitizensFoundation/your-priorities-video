90ae20e14570440c6f9ddb51e537ca6c3cb47f49
Robert Bjarnason <robert.bjarnason@gmail.com>
2018-08-09 14:30:01 +0000

    Various

--- client_app/src/actions/recording-flows.js
+++ client_app/src/actions/recording-flows.js
@@ -7,12 +7,15 @@ export const SET_VIDEO_RECORD = 'SET_VIDEO_RECORD';
 export const SET_VIDEO_PREVIEW = 'SET_VIDEO_PREVIEW';
 export const SET_VIDEO_POSTVIEW = 'SET_VIDEO_POSTVIEW';
 export const SET_VIDEO_EDIT_POST_DETAIL = 'SET_VIDEO_EDIT_POST_DETAIL';
+export const SET_VIDEO_UPLOAD = 'SET_VIDEO_UPLOAD';
+export const SET_VIDEO_FLOW_COMPLETED = 'SET_VIDEO_FLOW_COMPLETED';
 
 export const SET_AUDIO_RECORD = 'SET_AUDIO_RECORD';
 export const SET_AUDIO_PREVIEW = 'SET_AUDIO_PREVIEW';
 export const SET_AUDIO_POSTVIEW = 'SET_AUDIO_POSTVIEW';
-
 export const SET_AUDIO_EDIT_POST_DETAIL = 'SET_AUDIO_EDIT_POST_DETAIL';
+export const SET_AUDIO_UPLOAD = 'SET_AUDIO_UPLOAD';
+export const SET_AUDIO_FLOW_COMPLETED = 'SET_AUDIO_FLOW_COMPLETED';
 
 export const setRecordingVideoFlowToPreview = () => (dispatch) => {
   dispatch(recordingVideoFlowToPreview());
@@ -30,6 +33,14 @@ export const setRecordingVideoFlowEditPostDetail = () => (dispatch) => {
   dispatch(recordingVideoFlowEditPostDetail());
 };
 
+export const setRecordingVideoFlowUpload = () => (dispatch) => {
+  dispatch(recordingVideoFlowUpload());
+};
+
+export const setRecordingVideoFlowCompleted= () => (dispatch) => {
+  dispatch(recordingVideoFlowCompleted());
+};
+
 export const setRecordingAudioFlowToPreview = () => (dispatch) => {
   dispatch(recordingAudioFlowToPreview());
 };
@@ -44,6 +55,14 @@ export const setRecordingAudioFlowToPostview = () => (dispatch) => {
 
 export const setRecordingAudioFlowEditPostDetail = () => (dispatch) => {
   dispatch(recordingAudioFlowEditPostDetail());
+};
+
+export const setRecordingAudioFlowUpload = () => (dispatch) => {
+  dispatch(recordingAudioFlowUpload());
+};
+
+export const setRecordingAudioFlowCompleted= () => (dispatch) => {
+  dispatch(recordingAudioFlowCompleted());
 };
 
 const recordingVideoFlowToPreview = () => {
@@ -70,19 +89,31 @@ const recordingVideoFlowEditPostDetail = () => {
   };
 };
 
-const recordingVideoFlowToPreview = () => {
+const recordingVideoFlowUpload = () => {
+  return {
+    type: SET_VIDEO_UPLOAD
+  };
+};
+
+const recordingVideoFlowCompleted = () => {
+  return {
+    type: SET_VIDEO_FLOW_COMPLETED
+  };
+};
+
+const recordingAudioFlowToPreview = () => {
   return {
     type: SET_AUDIO_PREVIEW
   };
 };
 
-const recordingVideoFlowToRecord = () => {
+const recordingAudioFlowToRecord = () => {
   return {
     type: SET_AUDIO_RECORD
   };
 };
 
-const recordingVideoFlowToPostview = () => {
+const recordingAudioFlowToPostview = () => {
   return {
     type: SET_AUDIO_POSTVIEW
   };
@@ -94,3 +125,14 @@ const recordingAudioFlowEditPostDetail = () => {
   };
 };
 
+const recordingAudioFlowUpload = () => {
+  return {
+    type: SET_AUDIO_UPLOAD
+  };
+};
+
+const recordingAudioFlowCompleted = () => {
+  return {
+    type: SET_AUDIO_FLOW_COMPLETED
+  };
+};
